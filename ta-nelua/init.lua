407643642410ccb5c1f63fc59bde3d13d0adbf2a
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-02-16 22:57:23 -0300

    compile and run commands available :3

---
+++ ta-nelua/init.lua
@@ -0,0 +1,7 @@
+textadept.editing.comment_string.nelua = '--'
+textadept.run.compile_commands.nelua = function ()
+   return "nelua -q -b %p", io.get_project_root()
+end
+textadept.run.run_commands.nelua = function ()
+   return "nelua -q %p", io.get_project_root()
+end
