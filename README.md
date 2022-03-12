d2a0cee6724b9e5659ad02f2b73c10ba74245193
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2022-03-12 20:45:33 -0300

    lexer updated, with some cleanups, fixes and lua embedded syntax

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # ta-nelua
 
-** NOTE: This is now unmaintained, feel free to create a fork and continue it.**
-
 Lexer to add [Nelua](https://nelua.io/) support to [Textadept](https://foicica.com/textadept/) editor.
 
 ## How to install
@@ -11,20 +9,20 @@ Lexer to add [Nelua](https://nelua.io/) support to [Textadept](https://foicica.c
 * Move the ta-nelua directory to *_USERHOME*/modules/ directory;
 * Add this line to your *_USERHOME*/init.lua: ``require("ta-nelua")``;
 
-(*_USERHOME* is your textadept userhome directory, by default, ~/.textadept/ or  C:\Users\username\.textadept)
+> Note : *_USERHOME* it's your textadept userhome directory, by default: `~/.textadept/` on Linux and `C:\Users\your_username\.textadept` on Windows)
 
 ## Usage
 
 When opening any .nelua file, the nelua lexer will be selected automatically.
 
-To run or compile a file, you can:
-* use Ctrl+R to run; Shift+Ctrl+R to compile;
-* go to Tools -> Run to run; Tools -> Compile to compile
+To run or compile a file, you either:
+* use `Ctrl+R` to run; `Shift+Ctrl+R` to compile;
+* go to `Tools -> Run` to run; `Tools -> Compile` to compile
 
 If your file is on a VCS (Bazaar, Git, Mercurial, or SVN) directory, the root of the VCS project will be considered the working directory to run/compile.
 
-Consult *Compile, Run, and Build* section of [Textadept's Manual](https://foicica.com/textadept/manual.html) to more information.
+Consult *Compile, Run, and Build* section of [Textadept's Manual](https://foicica.com/textadept/manual.html) for more information.
 
 ## Preview
 
-![Textadept preview using base16-ashes-dark theme](ta_preview.jpg)
+![Textadept preview using base16-ashes-dark theme](ta_preview.png)
--- lexer/nelua.lua
+++ lexer/nelua.lua
@@ -1,142 +1,125 @@
--- Copyright 2020 André Luiz Alvares. See License.txt
+-- Copyright 2020-2022 André Luiz Alvares. See License.txt
 -- Nelua LPeg lexer.
 
--- This was written from scratch, using the Textadept's Lua
--- lexer from mitchell as guide
-
--- TODO: Update Lua libraries and other things once Lua 5.4 launches
-
 local lexer = require('lexer')
+
 local token, word_match = lexer.token, lexer.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
 local lex = lexer.new('nelua')
 
--- Whitespace.
-lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
+-- Whitespace
+local ws0 = lexer.space^0
+local ws1 = lexer.space^1
 
--- Keywords.
+-- Keywords --
 
--- Lua keywords from: https://www.lua.org/manual/5.3/manual.html#3.1
--- Nelua keywords from: [Nelua source code/nelua/syntaxdefs.lua at line 53](https://github.com/edubart/nelua-lang/blob/e74db9f729d49807cf5a072363f206fa0b26bf43/nelua/syntaxdefs.lua#L53)
-local keywords = token(lexer.KEYWORD, word_match[[
-  -- (ne)lua keywords
-  and break do else elseif end false for function if
-  in local nil not or repeat return then true until while
+local keywords = token(lexer.KEYWORD, word_match{
+  -- lua keywords: https://www.lua.org/manual/5.4/manual.html#3.1
+  'and', 'break', 'do', 'else', 'elseif', 'end',
+  'false', 'for', 'function', 'goto', 'if', 'in',
+  'local', 'nil', 'not', 'or', 'repeat', 'return',
+  'then', 'true', 'until', 'while',
 
-  -- Added in 5.2
-  goto
+  -- nelua keywords: https://github.com/edubart/nelua-lang/blob/af61898a8d90dc9736ab8f2e21e24a09a19b138d/docs/_includes/prismlangs.js#L61
+  'switch', 'case', 'continue', 'fallthrough', 'global', 'defer',
+})
 
-  -- nelua-only keywords
-  switch case continue global
-]])
 
-lex:add_rule('keyword', keywords)
+-- Types --
 
--- Types
--- from: [Nelua source code/nelua/typedefs.lua](https://github.com/edubart/nelua-lang/blob/e74db9f729d49807cf5a072363f206fa0b26bf43/nelua/typedefs.lua)
 local type_op = S(':@')
-lex:add_rule('type', token(lexer.OPERATOR, type_op) * lexer.space^0 * token(lexer.TYPE, lexer.word) * -(lexer.space^0 * P'(' * (lexer.any-P')')^0  * P')') )
 
--- Functions and deprecated functions
--- from: https://www.lua.org/manual/5.1/contents.html#index,
---       https://www.lua.org/manual/5.2/contents.html#index and
---       https://www.lua.org/manual/5.3/contents.html#index
+-- built-in and standard simple functions --
 
-local functions = token(lexer.FUNCTION, word_match[[
-  -- lua functions, some are available to nelua
-  assert collectgarbage dofile error getmetatable ipairs load
-  loadfile next pairs pcall print rawequal rawget rawset
-  require select setmetatable tonumber tostring type xpcall
-
-  -- Added in 5.2
-  rawlen
-
-  -- Added in 5.4, also available on nelua
-  warn
-
-  -- nelua functions
-  likely unlikely
-]])
-
-local deprecated_functions = token('deprecated_functions', word_match([[
-  -- Deprecated in 5.2
-  getfenv loadstring module setfenv unpack
-]]))
-
-lex:add_rule('function', functions + deprecated_functions)
-lex:add_style('deprecated_functions', lexer.STYLE_FUNCTION .. ',italics')
+-- nelua functions listed at libraries page: https://nelua.io/libraries/
+local functions = token(lexer.FUNCTION, word_match{
+  'require', 'print', 'panic', 'error', 'assert', 'check', 'likely', 'unlikely',
+  'arg', 'ipairs', 'mipairs', 'next', 'mnext', 'pairs', 'mpairs', 'select', 'tostring',
+  'tonumber', 'tointeger', 'warn'
+})
 
 -- Constants
-lex:add_rule('constant', token(lexer.CONSTANT, word_match[[
-  _G _VERSION
-  -- Added in 5.2
-  _ENV
-]]))
 
--- Lua libraries
-local lua_libraries = token('library', word_match[[
-  -- coroutine library
-  coroutine.create coroutine.resume coroutine.running coroutine.status coroutine.wrap coroutine.yield
-  -- Added in 5.3
-  coroutine.isyieldable
+-- Nelua libraries, from libraries page: https://nelua.io/libraries/
+local libraries = token('library', word_match{
+  -- io
+  'io.stderr', 'io.stdout', 'io.stdin', 'io.open', 'io.popen', 'io.close', 'io.flush', 'io.input', 'io.output',
+  'io.tmpfile', 'io.read', 'io.write', 'io.writef', 'io.printf', 'io.type', 'io.lines',
 
-  -- debug library
-  debug.debug debug.gethook debug.getinfo debug.getlocal debug.getmetatable debug.getregistry debug.getupvalue debug.sethook debug.setlocal debug.setmetatable debug.setupvalue debug.traceback
-  -- Added in 5.2
-  debug.getuservalue debug.setuservalue debug.upvalueid debug.upvaluejoin
+  -- filestream
+  'filestream._fromfp', 'filestream._getfp', 'filestream.open', 'filestream.flush', 'filestream.close', 'filestream.destroy',
+  'filestream.__close', 'filestream.seek', 'filestream.setvbuf', 'filestream.read', 'filestream.write', 'filestream.writef',
+  'filestream.lines', 'filestream.isopen', 'filestream.__tostring',
 
-  -- io library
-  io.close io.flush io.input io.lines io.open io.output io.popen io.read io.stderr io.stdin io.stdout io.tmpfile io.type io.write
+  -- math
+  'math.abs', 'math.floor', 'math.ifloor', 'math.ceil', 'math.iceil', 'math.round', 'math.trunc', 'math.sqrt', 'math.cbrt',
+  'math.exp', 'math.exp2', 'math.pow', 'math.log', 'math.cos', 'math.sin', 'math.tan', 'math.acos', 'math.asin', 'math.atan',
+  'math.atan2', 'math.cosh', 'math.sinh', 'math.tanh', 'math.log10', 'math.log2', 'math.acosh', 'math.asinh', 'math.atanh',
+  'math.deg', 'math.rad', 'math.sign', 'math.fract', 'math.mod', 'math.modf', 'math.fmod', 'math.frexp', 'math.ldexp', 'math.min',
+  'math.max', 'math.clamp', 'math.ult', 'math.tointeger', 'math.type', 'math.randomseed', 'math.random', 'math.pi', 'math.huge',
+  'math.mininteger', 'math.maxinteger', 'math.maxuinteger',
 
-  -- math library
-  math.abs math.acos math.asin math.atan math.ceil math.cos math.deg math.exp math.floor math.fmod math.huge math.log math.max math.min math.modf math.pi math.rad math.random math.randomseed math.sin math.sqrt math.tan
-  -- Added in 5.3
-  math.maxinteger math.mininteger math.tointeger math.type math.ult
+  -- memory
+  'memory.copy', 'memory.move', 'memory.set', 'memory.zero', 'memory.compare', 'memory.equals', 'memory.scan', 'memory.find',
+  'memory.spancopy', 'memory.spanmove', 'memory.spanset', 'memory.spanzero', 'memory.spancompare', 'memory.spanequals', 'memory.spanfind',
 
-  -- os library
-  os.clock os.date os.difftime os.execute os.exit os.getenv os.remove os.rename os.setlocale os.time os.tmpname
+  -- os
+  'os.clock', 'os.date', 'os.difftime', 'os.execute', 'os.exit', 'os.setenv', 'os.getenv', 'os.remove', 'os.rename',
+  'os.setlocale', 'os.timedesc', 'os.time', 'os.tmpname', 'os.now', 'os.sleep',
 
-  -- package library
-  package.config package.cpath package.loaded package.loadlib package.path package.preload package.searchers package.searchpath
+  -- string
+  'string.create', 'string.destroy', 'string.copy', 'string.byte', 'string.sub', 'string.subview', 'string.rep',
+  'string.reverse', 'string.upper', 'string.lower', 'string.char', 'string.format', 'string.len', 'string.fillcstring',
+  'string.span', 'string.__close', 'string.__atindex', 'string.__len', 'string.__concat', 'string.__eq', 'string.__lt',
+  'string.__le', 'string.__add', 'string.__sub', 'string.__mul', 'string.__div', 'string.__idiv', 'string.__tdiv', 'string.__mod',
+  'string.__tmod', 'string.__pow', 'string.__unm', 'string.__band', 'string.__bor', 'string.__bxor', 'string.__shl', 'string.__shr',
+  'string.__asr', 'string.__bnot', 'string.find', 'string.gmatch', 'string.gmatchview', 'string.gsub', 'string.match',
+  'string.matchview', 'string.pack', 'string.unpack', 'string.packsize',
 
-  -- string library
-  string.byte string.char string.dump string.find string.format string.gmatch string.gsub string.len string.lower string.match string.pack string.packsize string.rep string.reverse string.sub string.unpack string.upper
-  -- Added in 5.3
-  string.pack string.packsize string.unpack
+  -- stringbuilder
+  'stringbuilder.make', 'stringbuilder.destroy', 'stringbuilder.__close', 'stringbuilder.clear', 'stringbuilder.prepare',
+  'stringbuilder.commit', 'stringbuilder.rollback', 'stringbuilder.resize', 'stringbuilder.writebyte', 'stringbuilder.write',
+  'stringbuilder.writef', 'stringbuilder.view', 'stringbuilder.promote', 'stringbuilder.__len', 'stringbuilder.__tostring',
 
-  -- table library
-  table.concat table.insert table.pack table.remove table.sort table.unpack
-  -- Added in 5.3
-  table.move
+  -- traits
+  'traits.typeid', 'traits.typeinfo', 'traits.typeidof', 'traits.typeinfoof',
 
-  -- utf8 library
-  utf8.char utf8.charpattern utf8.codepoint utf8.codes utf8.len utf8.offset
-]])
+  -- utf8
+  'utf8.charpattern', 'utf8.char', 'utf8.codes', 'utf8.codepoint', 'utf8.offset', 'utf8.len',
 
-local lua_deprecated_libraries = token('deprecated_library', word_match[[
-  -- Deprecated (in 5.2) debug functions
-  debug.getfenv debug.setfenv
+  -- coroutine
+  'coroutine.destroy', 'coroutine.__close', 'coroutine.create', 'coroutine.push', 'coroutine.pop', 'coroutine.isyieldable',
+  'coroutine.resume', 'coroutine.spawn', 'coroutine.yield', 'coroutine.running', 'coroutine.status',
 
-  -- Deprecated (in 5.2, except .log10 which was deprecated on 5.3) math functions
-  math.atan2 math.cosh math.frexp math.ldexp math.log10 math.pow math.sinh math.tanh
+  -- hash
+  'hash.short', 'hash.long', 'hash.combine', 'hash.hash',
 
-  -- Deprecated (in 5.2) package functions
-  package.loaders package.seeall
+  -- vector
+  'vectorT.make', 'vectorT.clear', 'vectorT.destroy', 'vectorT.__close', 'vectorT.reserve', 'vectorT.resize', 'vectorT.copy',
+  'vectorT.push', 'vectorT.pop', 'vectorT.insert', 'vectorT.remove', 'vectorT.removevalue', 'vectorT.removeif', 'vectorT.capacity',
+  'vectorT.__atindex', 'vectorT.__len', 'vectorT.__convert',
 
-  -- Deprecated (in 5.2) table function
-  table.maxn
+  -- sequence
+  'sequence._init', 'sequence.make', 'sequence.clear', 'sequence.destroy', 'sequence.__close', 'sequence.reserve', 'sequence.resize',
+  'sequence.copy', 'sequence.push', 'sequence.pop', 'sequence.insert', 'sequence.remove', 'sequence.removevalue', 'sequence.removeif',
+  'sequence.capacity', 'sequence.__atindex', 'sequence.__len', 'sequence.__convert', 'sequence.unpack',
 
-  -- bit32 library was deprecated in 5.3
-  bit32.arshift bit32.band bit32.bnot bit32.bor bit32.btest bit32.bxor bit32.extract bit32.lrotate bit32.lshift bit32.replace bit32.rrotate bit32.rshift
-]])
+  -- list
+  'list.make', 'list.clear', 'list.destroy', 'list.__close', 'list.pushfront', 'list.pushback', 'list.insert', 'list.popfront',
+  'list.popback', 'list.find', 'list.erase', 'list.empty', 'list.__len', 'list.__next', 'list.__mnext', 'list.__pairs', 'list.__mpairs',
+  'list.__convert',
 
-lex:add_rule('library', lua_libraries + lua_deprecated_libraries)
-lex:add_style('library', lexer.STYLE_TYPE)
-lex:add_style('deprecated_library', lexer.STYLE_TYPE .. ',italics')
+  -- hashmap
+  'hashmap.make', 'hashmap.destroy', 'hashmap.__close', 'hashmap.clear', 'hashmap._find', 'hashmap.rehash', 'hashmap.reserve',
+  'hashmap._at', 'hashmap.__atindex', 'hashmap.peek', 'hashmap.remove', 'hashmap.loadfactor', 'hashmap.bucketcount', 'hashmap.capacity',
+  'hashmap.__len', 'hashmap.__pairs', 'hashmap.__mpairs', 'hashmap._next_node', 'hashmap.__next', 'hashmap.__mnext',
+
+  -- TODO: Allocators
+})
+
 
 -- Identifiers.
-lex:add_rule('identifier', token(lexer.IDENTIFIER, lexer.word))
 
 -- Strings.
 local longstring = lpeg.Cmt(
@@ -147,40 +130,76 @@ local longstring = lpeg.Cmt(
   end
 )
 
-lex:add_rule('string', token(lexer.STRING, lexer.delimited_range("'") + lexer.delimited_range('"')) + token('longstring', longstring))
-lex:add_style('longstring', lexer.STYLE_STRING)
-
 -- Comments.
-lex:add_rule('comment', token(lexer.COMMENT, '--' * (longstring + lexer.nonnewline^0)))
 
 -- Numbers.
-local nelua_suffixes = word_match[[
-  -- Number suffixes
-  _i _integer _u _uinteger _n _number _b _byte _is _isize
-  _i8 _int8 _i16 _int16 _i32 _int32 _i64 _int64 _us _usize
-  _u8 _uint8 _u16 _uint16 _u32 _uint32 _u64 _uint64
-  _f32 _float32 _f64 _float64
+local nelua_suffixes = word_match{
+  '_i', '_integer',
+  '_u', '_uinteger',
+  '_n', '_number',
+  '_b', '_byte',
+  '_is', '_isize',
+  '_i8', '_int8',
+  '_i16', '_int16',
+  '_i32', '_int32',
+  '_i64', '_int64',
+  '_i128', '_int128',
+  '_us', '_usize',
+  '_u8', '_uint8',
+  '_u16', '_uint16',
+  '_u32', '_uint32',
+  '_u64', '_uint64',
+  '_u128', '_uint128',
+  '_f32', '_float32',
+  '_f64', '_float64',
+  '_f128', '_float128',
 
   -- C primitive suffixes
-  _cchar _cschar _cshort _cint _clong _clonglong _cptrdiff
-  _cuchar _cushort _cuint _culong _culonglong _csize _clongdouble
-]]
+  '_cshort',
+  '_cint',
+  '_clong',
+  '_clonglong',
+  '_cptrdiff',
+  '_cchar',
+  '_cschar',
+  '_cuchar',
+  '_cushort',
+  '_cuint',
+  '_culong',
+  '_culonglong',
+  '_csize',
+  '_clongdouble',
+  '_cstring',
+}
 
 local integer = P('-')^-1 * (lexer.hex_num + lexer.dec_num)
+
+-- preprocessor
+local preprocessor_line = token('preprocessor_token', P'##')
+local preprocessor_start = token('preprocessor_token', P'#|' + '#[' + (P'##[' * P'='^0 * '['))
+local preprocessor_end  = token('preprocessor_token', P'|#' + "]#" +   (P']' * P'='^0 * ']'))
+
+-- rules
+
+lex:add_rule('whitespace', token(lexer.WHITESPACE, ws1))
+lex:add_rule('keyword', keywords)
+lex:add_rule('type', token(lexer.OPERATOR, type_op) * ws0 * token(lexer.TYPE, lexer.word) * -(ws0 * P'(' * (lexer.any-P')')^0  * P')') )
+lex:add_rule('function', functions)
+lex:add_rule('constant', token(lexer.CONSTANT, P'_VERSION'))
+lex:add_style('library', lexer.STYLE_TYPE)
+lex:add_rule('identifier', token(lexer.IDENTIFIER, lexer.word))
+lex:add_rule('string', token(lexer.STRING, lexer.range("'") + lexer.range('"')) + token('longstring', longstring))
+lex:add_style('longstring', lexer.STYLE_STRING)
+lex:add_rule('comment', token(lexer.COMMENT, '--' * (longstring + lexer.nonnewline^0)))
 lex:add_rule('number', token(lexer.NUMBER, (lexer.float + integer) * (nelua_suffixes)^0))
-
--- Labels
 lex:add_rule('label', token(lexer.LABEL, '::' * lexer.word * '::'))
+lex:add_rule('preprocessor', preprocessor_line + preprocessor_start + preprocessor_end)
 
--- Prerocessor
-lex:add_rule('preprocessor', token('preprocessor_token', (P('##[[') + '##' + ']]' + '#|' + '|#' + "#[" + "]#")^1))
-lex:add_style('preprocessor_token', lexer.STYLE_PREPROCESSOR .. ',bold')
-
--- Annotations
-lex:add_rule('annotation', token(lexer.PREPROCESSOR, lexer.delimited_range("<>", true, false, true)))
-
--- Operators.
+lex:add_rule('annotation', token(lexer.PREPROCESSOR, P'<' * -lexer.space * (lexer.nonnewline - P' >' - P'>')^1 * '>'))
+lex:add_style('annotation', lexer.styles.class)
 lex:add_rule('operator', token(lexer.OPERATOR, '..' + S('+-*/%^#=<>&|~;,.{}[]()$') + type_op))
+lex:add_rule('library', libraries)
+lex:add_style('preprocessor_token', lexer.STYLE_PREPROCESSOR .. {bold = true})
 
 -- Fold points.
 local function fold_longcomment(text, pos, line, s, symbol)
@@ -195,9 +214,9 @@ end
 lex:add_fold_point(lexer.KEYWORD, 'if', 'end')
 lex:add_fold_point(lexer.KEYWORD, 'do', 'end')
 lex:add_fold_point(lexer.KEYWORD, 'switch', 'end')
+lex:add_fold_point(lexer.KEYWORD, 'defer', 'end')
 lex:add_fold_point(lexer.KEYWORD, 'function', 'end')
 lex:add_fold_point(lexer.KEYWORD, 'repeat', 'until')
--- TODO: in the future, defer<--->end should be included
 lex:add_fold_point(lexer.COMMENT, '[', fold_longcomment)
 lex:add_fold_point(lexer.COMMENT, ']', fold_longcomment)
 lex:add_fold_point(lexer.COMMENT, '--', lexer.fold_line_comments('--'))
@@ -206,5 +225,10 @@ lex:add_fold_point('longstring', '[', ']')
 lex:add_fold_point(lexer.OPERATOR, '(', ')')
 lex:add_fold_point(lexer.OPERATOR, '[', ']')
 lex:add_fold_point(lexer.OPERATOR, '{', '}')
+
+-- embed lua
+local lua = lexer.load('lua')
+lex:embed(lua, preprocessor_start, preprocessor_end)
+lex:embed(lua, preprocessor_line, lexer.newline)
 
 return lex
